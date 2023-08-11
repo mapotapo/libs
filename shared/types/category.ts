815319f89ebf7fa165105e2308f985b28128d24f
Hung <txhung08@gmail.com>
2023-08-11 21:30:24 +0100

    updated categorydto

--- shared/types/category.ts
+++ shared/types/category.ts
@@ -1,11 +1,19 @@
 import { ProductDto } from './product';
 
+export type Icon = {
+  default: string;
+  hover?: string | null;
+  selected?: string | null;
+};
+
 export type CategoryDto = {
   id: string;
   name: string;
-  icon: string;
+  icon: Icon;
   description?: string | null;
 };
+
+export type CategoryNoIdDto = Omit<CategoryDto, 'id'>;
 
 export type CategoryWithProductsDto = CategoryDto & {
   products: ProductDto[];
